4ddce3ed82cda78e55608b0222388d423a7a07be
IT20230692 <IT20230692@my.sliit.lk>
2023-01-20 15:48:24 +0530

    create delete backend

--- NODE-REST-API/routes/post.js
+++ NODE-REST-API/routes/post.js
@@ -14,21 +14,34 @@ router.post('/', async (req, res) => {
 
 //update a post
 router.put('/:id', async (req, res) => {
-    try{
-        const post =  await Post.findById(req.params.id);
-        if(post.userId === req.body.userId){
-            await post.updateOne({ $set: req.body})
-            res.status(200).json("The post has been updated")
-        }else{
-            res.status(403).json("You can update only our posts...")
-        }
-    }catch (err) {
-        res.status(500).json(err);
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json('The post has been updated');
+    } else {
+      res.status(403).json('You can update only our posts...');
     }
-})
-
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //delete a post
+router.delete('/:id', async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.userId === req.body.userId) {
+        await post.deleteOne({ $set: req.body });
+        res.status(200).json('The post has been deleted');
+      } else {
+        res.status(403).json('You can delete only our posts...');
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 //like a post
 //get a post
 //get timeline
