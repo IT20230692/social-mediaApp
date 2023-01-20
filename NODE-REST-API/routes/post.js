dfc0a45faa35182fa2f85c1a99fcdf3136b079d2
IT20230692 <IT20230692@my.sliit.lk>
2023-01-20 15:47:26 +0530

    create update post backend

--- NODE-REST-API/routes/post.js
+++ NODE-REST-API/routes/post.js
@@ -13,7 +13,19 @@ router.post('/', async (req, res) => {
 });
 
 //update a post
-
+router.put('/:id', async (req, res) => {
+    try{
+        const post =  await Post.findById(req.params.id);
+        if(post.userId === req.body.userId){
+            await post.updateOne({ $set: req.body})
+            res.status(200).json("The post has been updated")
+        }else{
+            res.status(403).json("You can update only our posts...")
+        }
+    }catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 
 //delete a post
