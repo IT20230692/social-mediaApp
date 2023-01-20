6e025aac3a74dd4cab6fc97f99efeff7c98f9120
IT20230692 <IT20230692@my.sliit.lk>
2023-01-20 12:37:53 +0530

    create update user backend

--- NODE-REST-API/routes/users.js
+++ NODE-REST-API/routes/users.js
@@ -1,7 +1,37 @@
-const router =require("express").Router();
+const router = require('express').Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
-router.get("/", (req,res)=>{
-    res.send("hey users")
-})
+//Update user
+router.put('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json('Account updated successfully...');
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json('You can update your account only!');
+  }
+});
 
-module.exports = router
+//Delete User
+
+///Get User
+
+//Follow User
+
+//Unfollow User
+
+module.exports = router;
