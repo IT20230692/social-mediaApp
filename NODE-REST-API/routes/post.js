8a8544154a040220b6ab1b54c6231b12e6fced91
IT20230692 <IT20230692@my.sliit.lk>
2023-01-20 15:58:50 +0530

    create like dislike post backend

--- NODE-REST-API/routes/post.js
+++ NODE-REST-API/routes/post.js
@@ -29,20 +29,35 @@ router.put('/:id', async (req, res) => {
 
 //delete a post
 router.delete('/:id', async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id);
-      if (post.userId === req.body.userId) {
-        await post.deleteOne({ $set: req.body });
-        res.status(200).json('The post has been deleted');
-      } else {
-        res.status(403).json('You can delete only our posts...');
-      }
-    } catch (err) {
-      res.status(500).json(err);
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne({ $set: req.body });
+      res.status(200).json('The post has been deleted');
+    } else {
+      res.status(403).json('You can delete only our posts...');
     }
-  });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-//like a post
+//like/deislike a post
+router.put('/:id/like', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json('The post has been liked...');
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json('The post has been disliked');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //get a post
 //get timeline
 module.exports = router;
